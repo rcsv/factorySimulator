85d8cd34898b104b6ee1108a6305a6b83aa744ca
rcsv <Awane.Tomohiro@me.com>
2013-07-01 02:46:30 +0900

    Create Surveillance.java

---
+++ src/org/rcsvp/pls/Surveillance.java
@@ -0,0 +1,21 @@
+package org.rcsvp.pls;
+
+/**
+ * どう動くか見物。今回の肝となる存在。
+ * 
+ * @author Tomohiro AWANE <Awane.Tomohiro@me.com>
+ *
+ */
+public class Surveillance extends PlsAbst implements ISurveillance {
+
+  public Surveillance(String name) {
+		super(name);
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
