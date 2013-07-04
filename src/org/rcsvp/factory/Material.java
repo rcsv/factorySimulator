0273fd96f8aa5d3dc97a258e98295364301470ef
rcsv <Awane.Tomohiro@me.com>
2013-07-05 08:04:48 +0900

    Refactoring about IMaterial implementation
    材料を表現しているクラスを再考。こちら側にツール加工、および検証に関するメソッドを用意した。Procedure から呼ばれる。

--- src/org/rcsvp/factory/Material.java
+++ src/org/rcsvp/factory/Material.java
@@ -1,15 +1,53 @@
-package org.rcsvp.factory;
+package org.rcsvp.factory ;
 
-import org.rcsvp.factory.common.IMaterial;
+import java.util.* ;
+
+import org.rcsvp.Logger ;
+import org.rcsvp.factory.common.IDisposable ;
+import org.rcsvp.factory.common.IMaterial ;
+import org.rcsvp.factory.common.IVerify ;
 
 public class Material implements IMaterial {
 
 	private String name ;
+
+	private Stack<IMaterial> bom ;
 	
-	public Material( String name ) {
+	private Queue<IDisposable> fabricHistory ;
+	
+	private Queue<IVerify> verifyHistory ;
+	
+
+	public Material(String name) {
 		this.name = name ;
+		this.bom = new Stack<IMaterial>() ;
+		this.fabricHistory = new LinkedList<IDisposable>() ;
+		this.verifyHistory = new LinkedList<IVerify>() ;
+	}
+
+	@Override
+	public String toString() {
+		return this.name ;
+	}
+
+	@Override
+	public void append(IMaterial other) {
+		Logger.debugWrite( this.name + " : append other material" + other.toString());
+		this.bom.push(other) ;
+	}
+
+	@Override
+	public boolean fabricate(IDisposable tools) {
+		Logger.debugWrite( this.name + " : fabricated by " + tools.toString());
+		this.fabricHistory.add(tools);
+		return tools.use(this) ;
+		
 	}
 	
 	@Override
-	public String toString() { return this.name ; }
+	public boolean validate( IVerify verification ) {
+		Logger.debugWrite( this.name + " : verify about " + verification);
+		this.verifyHistory.add(verification);
+		return verification.check(this) ;
+	}
 }
