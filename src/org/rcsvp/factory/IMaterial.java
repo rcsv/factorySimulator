41936d0bb8e8e35e4e0969733f6e057677eacd8a
rcsv <Awane.Tomohiro@me.com>
2013-07-06 04:34:24 +0900

    Add comment in interface.

--- src/org/rcsvp/factory/IMaterial.java
+++ src/org/rcsvp/factory/IMaterial.java
@@ -1,5 +1,20 @@
 package org.rcsvp.factory ;
 
+/**
+ * Interface IMaterial represents a material using in a factory. Material maybe
+ * Cupper, Rare-Metal, sub-assy, or all. Factory must create product from these
+ * materials.
+ * 
+ * IMaterial prepare several method in it.
+ * 
+ * 1. Append other material when create sub assembly parts.
+ * 2. fabricate with disposable tools.
+ * 3. validate result of fabrication.
+ * 
+ * @author Rcsvp.org
+ * @date Jul 6, 2013
+ * 
+ */
 public interface IMaterial {
 
 	void append(IMaterial other) ;
