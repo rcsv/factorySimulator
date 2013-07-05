3faa05493cec541344ca2faaa5e56a0c2d4e093b
rcsv <Awane.Tomohiro@me.com>
2013-07-05 21:25:44 +0900

    Add comment

--- src/org/rcsvp/factory/AbstFacilities.java
+++ src/org/rcsvp/factory/AbstFacilities.java
@@ -1,77 +1,100 @@
-package org.rcsvp.factory;
+package org.rcsvp.factory ;
 
-import org.rcsvp.Logger;
-import org.rcsvp.factory.common.*;
+import org.rcsvp.Logger ;
+import org.rcsvp.factory.common.* ;
 
+/**
+ * AbstFacility class represent a workflow of this simulation object. there are
+ * initialize, register child node and Thread start. After starting as a thread,
+ * it step following step: 1. bootUp, 2. routine, 3. finish process. it'll
+ * finish after all method are passed completely.
+ * 
+ * This pattern will be going to apply IFactory, IProductionLine, ILabor, and
+ * IProcedure implementation classes.
+ * 
+ * @author Rcsvp.org
+ * @date Jul 5, 2013
+ * 
+ */
 public abstract class AbstFacilities implements IRegistrable, IParent {
 
-	protected IControlCenter cc;
-	protected String name;
+	/**
+	 * A instance of IControlCenter it must be call method
+	 * {@link IControlCenter#notify(org.rcsvp.factory.common.IControlCenter.IAlertBox)}
+	 * when condition of each objects are changed.
+	 */
+	protected IControlCenter cc ;
+	
+	/**
+	 * Stored object name for HashMap's key of parent node.
+	 */
+	protected String name ;
 
-	protected volatile boolean powerOff = false;
+	protected volatile boolean powerOff = false ;
 
-	protected GeneralStatus status;
+	protected GeneralStatus status ;
 
-	protected long tactTime;
+	protected long tactTime ;
 
 	public AbstFacilities(String name, long tactTime) {
-		this.name = name;
-		this.tactTime = tactTime;
-		this.status = GeneralStatus.Ready;
+		this.name = name ;
+		this.tactTime = tactTime ;
+		this.status = GeneralStatus.Ready ;
 	}
 
 	@Override
 	public IStatusEnum getStatus() {
-		return this.status;
+		return this.status ;
 	}
 
 	@Override
 	public void run() {
-		Logger.debugWrite(this.name + " : start running.");
-		this.status = GeneralStatus.Working;
-		cc.notify(new AlertBox(this));
+		Logger.debugWrite(this.name + " : start running.") ;
+		this.status = GeneralStatus.Working ;
+		cc.notify(new AlertBox(this)) ;
 
-		bootUp();
+		bootUp() ;
 
 		while (!powerOff && otherCheck()) {
-			
+
 			try {
-				Thread.sleep(this.tactTime * cc.getTimeScale());
+				Thread.sleep(this.tactTime * cc.getTimeScale()) ;
 			} catch (InterruptedException e) {
-				e.printStackTrace();
+				e.printStackTrace() ;
 			}
 
-			Logger.debugWrite( this.name + " : working job..." );
-			routines();
+			Logger.debugWrite(this.name + " : working job...") ;
+			routines() ;
 
 		}
 
-		this.status = GeneralStatus.NormallyShutdown;
-		cc.notify(new AlertBox(this));
-		finishProcess();
-		Logger.debugWrite(this.name + " : finish work.");
+		this.status = GeneralStatus.NormallyShutdown ;
+		cc.notify(new AlertBox(this)) ;
+		finishProcess() ;
+		Logger.debugWrite(this.name + " : finish work.") ;
 	}
 
-	protected abstract void bootUp();
+	protected abstract void bootUp() ;
 
-	protected abstract boolean otherCheck();
+	protected abstract boolean otherCheck() ;
 
-	protected abstract void routines();
+	protected abstract void routines() ;
 
-	protected abstract void finishProcess();
+	protected abstract void finishProcess() ;
 
 	@Override
 	public void setControlCenter(IControlCenter cc) {
-		this.cc = cc;
+		this.cc = cc ;
+		cc.notify(new AlertBox(this)) ;
 	}
 
 	@Override
 	public void shutdown(IStatusEnum status) {
-		this.powerOff = true;
+		this.powerOff = true ;
 	}
 
 	@Override
 	public String toString() {
-		return this.name;
+		return this.name ;
 	}
 }
--- src/org/rcsvp/factory/AlertBox.java
+++ src/org/rcsvp/factory/AlertBox.java
@@ -1,31 +1,31 @@
-package org.rcsvp.factory;
+package org.rcsvp.factory ;
 
-import org.rcsvp.factory.common.IControlCenter.IAlertBox;
-import org.rcsvp.factory.common.IRegistrable;
-import org.rcsvp.factory.common.IRegistrable.*;
+import org.rcsvp.factory.common.IControlCenter.IAlertBox ;
+import org.rcsvp.factory.common.IRegistrable ;
+import org.rcsvp.factory.common.IRegistrable.* ;
 
 public class AlertBox implements IAlertBox {
 
-	private IStatusEnum status;
-	private IRegistrable object;
+	private IStatusEnum status ;
+	private IRegistrable object ;
 
 	public AlertBox(IStatusEnum stat, IRegistrable target) {
-		status = stat;
-		object = target;
+		status = stat ;
+		object = target ;
 	}
 
 	public AlertBox(IRegistrable target) {
-		status = target.getStatus();
-		object = target;
+		status = target.getStatus() ;
+		object = target ;
 	}
 
 	@Override
 	public IRegistrable getTarget() {
-		return object;
+		return object ;
 	}
 
 	public IStatusEnum getStatus() {
-		return status;
+		return status ;
 	}
 
 }
