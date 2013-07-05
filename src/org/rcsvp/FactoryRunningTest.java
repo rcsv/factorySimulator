d911a37f783189eab58fff162422a6cc867ca124
rcsv <Awane.Tomohiro@me.com>
2013-07-05 21:09:29 +0900

    add scinario to the check fabrication

--- src/org/rcsvp/FactoryRunningTest.java
+++ src/org/rcsvp/FactoryRunningTest.java
@@ -1,65 +1,71 @@
-package org.rcsvp;
+package org.rcsvp ;
 
-import org.rcsvp.factory.*;
-import org.rcsvp.factory.common.*;
+import org.rcsvp.factory.* ;
+import org.rcsvp.factory.common.* ;
 
 public class FactoryRunningTest {
 
 	public static void main(String[] args) {
-		Logger.lv = Logger.VerboseLevel.Debug;
+		Logger.lv = Logger.VerboseLevel.Info ;
 
-		Logger.debugWrite(" - - - START  - - - ");
+		Logger.debugWrite(" - - - START  - - - ") ;
 
 		//
 		// Generate Factory
 		//
-		IFactory factory = new Factory("Factory_1", 2);
+		IFactory factory = new Factory("FABRIC", 2) ;
+
+		IControlCenter cc = ControlCenter.getInstance("FABRIC") ;
+		cc.setTimeScale(1) ;
 
 		//
 		// Create labor and register
 		//
-		factory.register(new Labor("Kazuto UEHARA", 1));
+		factory.register(new Labor("Kazuto UEHARA", 1)) ;
 
-		// tactTime 1 sec, norma = 20.
-		IProductionLine pl1 = new ProductionLine("Line 1", 1, 20);
+		// tactTime 1 second, norm = 20.
+		IProductionLine pl1 = new ProductionLine("Line 1", 1, 20000) ;
 
-		factory.register(pl1);
+		factory.register(pl1) ;
 
-		IProcedure proc11 = new Procedure("Proc11");
-		IProcedure proc12 = new Procedure("Proc12");
-		IProcedure proc13 = new Procedure("Proc13");
+		IProcedure proc11 = new Procedure("Proc11") ;
+		IProcedure proc12 = new Procedure("Proc12") ;
+		IProcedure proc13 = new Procedure("Proc13") ;
+		IProcedure proc14 = new Procedure("Proc14") ;
+		IProcedure proc15 = new Procedure("Proc15") ;
 
-		pl1.register(proc11);
-		pl1.register(proc12);
-		pl1.register(proc13);
-		
-		IShelf sh11 = new Shelf( "Shelf for Proc11", 100 ) ;
-		proc11.register ( sh11 ) ;
+		pl1.register(proc11) ;
+		pl1.register(proc12) ;
+		pl1.register(proc13) ;
+		pl1.register(proc14) ;
+		pl1.register(proc15) ;
 
-		/*
-		 * 
-		 * 
-		 * // // Create Procedure and Register into Production Line //
-		 * IProcedure w11 = new Procedure("Procedure 1-1") ; IProcedure w12 =
-		 * new Procedure("Procedure 1-2") ; IProcedure w13 = new
-		 * Procedure("Procedure 1-3") ; IProcedure w14 = new
-		 * Procedure("Procedure 1-4") ;
-		 * 
-		 * pl1.register(w11); pl1.register(w12); pl1.register(w13);
-		 * pl1.register(w14);
-		 * 
-		 * IProcedure w21 = new Procedure("Procedure 2-1") ; IProcedure w22 =
-		 * new Procedure("Procedure 2-2") ; IProcedure w23 = new
-		 * Procedure("Procedure 2-3") ; IProcedure w24 = new
-		 * Procedure("Procedure 2-4") ;
-		 * 
-		 * pl1.register(w21); pl1.register(w22); pl1.register(w23);
-		 * pl1.register(w24);
-		 */
-		Thread x = new Thread(factory); 
-		x.setName("Factory Thread");
-		x.start();
-		Logger.debugWrite(" - - - FINISH - - - ");
+		IShelf sh11 = new Shelf("Shelf for Proc11", 60) ;
+		proc11.register(sh11) ;
+
+		IShelf sh12_1 = new Shelf("Shelf for Proc12#1", 20) ;
+		IShelf sh12_2 = new Shelf("Shelf for Proc12#2", 20) ;
+		proc12.register(sh12_1) ;
+		proc12.register(sh12_2) ;
+
+		//
+		// Create Disposable Tools and Register.
+		//
+		IDisposable d1 = new Disposable("Punch Tool", 100) ;
+		IDisposable d3 = new Disposable("some  Tool", 20) ;
+
+		proc11.register(d1) ;
+		proc13.register(d3) ;
+
+		IVerify check1 = new Verify("Length Check", new Tolerance(
+				ITolerance.ToleranceType.Scantling, 10, 10)) ;
+
+		proc11.register(check1) ;
+
+		Thread x = new Thread(factory) ;
+		x.setName("Factory Thread") ;
+		x.start() ;
+		Logger.debugWrite(" - - - FINISH - - - ") ;
 	}
 
 }
