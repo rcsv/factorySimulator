47cdc1dd3039dab2f63a176041ac297cb75f77e7
rcsv <Awane.Tomohiro@me.com>
2013-07-06 11:55:44 +0900

    add description

--- src/org/rcsvp/factory/ICare.java
+++ src/org/rcsvp/factory/ICare.java
@@ -1,5 +1,16 @@
 package org.rcsvp.factory ;
 
+/**
+ * ICare interface represents a "need care" to ILabor. To repair broken tool, to
+ * reload empty material shelves, ILabor implemented instance send
+ * <code>care ()</code> method of this interface.
+ * 
+ * @see ILabor
+ * 
+ * @author Rcsvp.org
+ * @date Jul 6, 2013
+ * 
+ */
 public interface ICare {
 
 	boolean care() ;
