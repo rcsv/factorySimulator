af6274d4d5ce62c8a63118706b3233b923626a7f
rcsv <Awane.Tomohiro@me.com>
2013-07-06 16:57:46 +0900

    Add description into IProcedure interface

--- src/org/rcsvp/factory/IProcedure.java
+++ src/org/rcsvp/factory/IProcedure.java
@@ -1,8 +1,17 @@
 package org.rcsvp.factory ;
 
 /**
+ * IProcedure interface represents a procedure of a Production Line. Fabricate
+ * material with plenty of method including press, bend, slice, punch and burn.
+ * Each procedure belongs to same production line take same time to fabrication.
+ * it is tact time. In factory simulator its value is given to its instances
+ * from every IProductionLine instance.
+ * 
+ * In Factory Simulator, a procedure has some IShelf for supply materials, some
+ * disposable tools, and lots of verification items.
  * 
  * @author Rcsvp.org
+ * @date Jul 6, 2013
  * 
  */
 public interface IProcedure extends IRegistrable, IParent {
