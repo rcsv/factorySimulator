01c929c95693a6cf7d20cc7208f516b173ad9306
rcsv <Awane.Tomohiro@me.com>
2013-07-06 13:18:26 +0900

    Add description into IControlCenter

--- src/org/rcsvp/factory/IControlCenter.java
+++ src/org/rcsvp/factory/IControlCenter.java
@@ -4,6 +4,18 @@ import java.util.Map ;
 
 import org.rcsvp.factory.IRegistrable.* ;
 
+/**
+ * IControlCenter represents a management-room of a factory. It can be checked
+ * all status in a factory's situation. Worker instance that implemented ILabor
+ * interface check and action triggered by it.
+ * 
+ *  {$Id$}
+ * 
+ * @see ILabor
+ * @author Rcsvp.org
+ * @date Jul 6, 2013
+ * 
+ */
 public interface IControlCenter {
 
 	interface IAlertBox {
