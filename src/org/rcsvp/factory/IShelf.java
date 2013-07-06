bcf01440a4ce168d9def2c35c95aed289a1ec9ff
rcsv <Awane.Tomohiro@me.com>
2013-07-06 17:20:31 +0900

    Add description

--- src/org/rcsvp/factory/IShelf.java
+++ src/org/rcsvp/factory/IShelf.java
@@ -1,5 +1,11 @@
 package org.rcsvp.factory ;
 
+/**
+ * IShelf represents a material shelf.
+ * @author Rcsvp.org
+ * @date   Jul 6, 2013
+ *
+ */
 public interface IShelf extends IRegistrable, ICare {
 
 	IMaterial getMaterial() ;
