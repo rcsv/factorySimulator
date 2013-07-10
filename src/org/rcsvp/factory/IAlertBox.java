d3031c024a680cc13fdd3959bd6b085ac0d74890
rcsv <Awane.Tomohiro@me.com>
2013-07-10 13:25:21 +0900

    Dettach IAlertBox from IControlCenter

---
+++ src/org/rcsvp/factory/IAlertBox.java
@@ -0,0 +1,24 @@
+package org.rcsvp.factory ;
+
+import org.rcsvp.factory.IRegistrable.IStatusEnum ;
+
+/**
+ * IAlertBox represents a status notification unit in ControlCenter.
+ * IControlCenter use it when it notify each unit status to labor.
+ *
+ * @author Rcsvp.org
+ * @date Jul 10, 2013
+ *
+ */
+public interface IAlertBox {
+
+  /**
+   * Labor need to know the address of facility when warned via AlertBox.
+   *
+   * @return a reference of facility address that have a problem.
+   */
+  IRegistrable getTarget () ;
+  
+  IStatusEnum getStatus () ;
+  
+}
