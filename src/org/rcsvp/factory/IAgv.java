96e62804d9c50eb69a75625bc6c677393b197589
rcsv <Awane.Tomohiro@me.com>
2013-07-10 13:05:00 +0900

    Create IAgv.java

---
+++ src/org/rcsvp/factory/IAgv.java
@@ -0,0 +1,30 @@
+package org.rcsvp.factory ;
+
+/**
+ * IAgv is a simple interface for representation, a Automated Guided Vehicle
+ * (AGV). His purpose that supply materials to a procedures.
+ * 
+ * An automated guided vehicle or automatic guided vehicle (AGV) is a mobile
+ * robot that follows makers or wires in the floor, or uses vision or lasers.
+ * They are most often used in industrial applications to move materials around
+ * a manufacturing facility or a warehouse. Application of the automatic guided
+ * vehicle has broadened during the late 20th century.
+ * 
+ * ## Introduction Automated guided vehicles (AGVs) increase efficiency and
+ * reduce costs by helping to automate a manufacturing facility or warehouse. The
+ * first AGV was invented by Barrett Electronics in 1953. The AGV can tow
+ * objects behind them in trailers to which they can autonomously attach. The
+ * trailers can be used to move raw materials or finished product. The AGV can
+ * also store objects on a bed. The objects can be placed on a set of motorized
+ * rollers (conveyor) and then pushed off by reversing them. AGVs are employed
+ * in nearly every industry, including, plup, paper, metals, newspaper, and
+ * general manufacturing. Transporting materials such as food, linen or medicine
+ * in hospitals is also done.
+ * 
+ * @author Rcsvp.org
+ * @date Jul 6, 2013
+ * 
+ */
+public interface IAgv extends IRegistrable {
+
+}
