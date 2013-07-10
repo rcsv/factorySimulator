d4b77460abe3889ac132edb9fea44e808587f413
rcsv <Awane.Tomohiro@me.com>
2013-07-11 00:10:50 +0900

    Add Comment

--- src/org/rcsvp/factory/IMaterial.java
+++ src/org/rcsvp/factory/IMaterial.java
@@ -1,25 +1,44 @@
 package org.rcsvp.factory ;
 
 /**
- * Interface IMaterial represents a material using in a factory. Material maybe
- * Cupper, Rare-Metal, sub-assy, or all. Factory must create product from these
- * materials.
+ * Interface IMaterial represents a material used in a factory. Material maybe
+ * Cupper, Rare metal, sub-assembly or full production. Factory must create a
+ * product from these materials.
  * 
- * IMaterial prepare several method in it.
+ * IMaterial prepare several method on it.
  * 
- * 1. Append other material when create sub assembly parts.
- * 2. fabricate with disposable tools.
- * 3. validate result of fabrication.
+ * 1. Append itself to other material when create sub assembly parts. 2.
+ * Fabricate with disposable tools. 3. Validate result of fabrication.
  * 
  * @author Rcsvp.org
- * @date Jul 6, 2013
+ * @date Jul, 6, 2013
  * 
  */
 public interface IMaterial {
 
+	/**
+	 * Append other Material.
+	 * 
+	 * @param other
+	 */
 	void append(IMaterial other) ;
 
+	/**
+	 * fabricate by Procedure using Disposable tool.
+	 * 
+	 * @param tools
+	 *            Disposable tool to use
+	 * @return success or failure fabrication by disposable tool.
+	 */
 	boolean fabricate(IDisposable tools) ;
 
+	/**
+	 * Verification by check content.
+	 * 
+	 * @param verification
+	 *            a test.
+	 * @return true when a test succeed or return false when cannot pass the
+	 *         exam.
+	 */
 	boolean validate(IVerify verification) ;
 }
