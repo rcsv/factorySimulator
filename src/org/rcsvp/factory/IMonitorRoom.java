6018a8ee8e264bed9f1731c5630b01001e48819c
rcsv <Awane.Tomohiro@me.com>
2013-07-11 23:58:50 +0900

    MonitorRoom のチェックイン
    旧 IControlCenter の IMonitorRoom をチェックイン。今のところ名称以外の変更は無し

---
+++ src/org/rcsvp/factory/IMonitorRoom.java
@@ -0,0 +1,37 @@
+package org.rcsvp.factory ;
+
+import java.util.Map ;
+
+/**
+ * IMonitorRoom (ex IControlCenter) represents a management-room of a factory.
+ * It can be checked all status in a factory's situation. Worker instance that
+ * implemented ILabor interface check and action triggered by it.
+ * 
+ * @author Rcsvp.org
+ * @date Jul 6, 2013
+ * 
+ */
+public interface IMonitorRoom {
+
+	/**
+	 * get Console Information and a reference of facility that have a problem.
+	 * 
+	 * @return a name and reference of facility.
+	 */
+	Map<String, IAlertBox> getConsole() ;
+
+	/**
+	 * set factory.
+	 * 
+	 * @param factory
+	 */
+	void setFactory(IFactory factory) ;
+
+	/**
+	 * set reference and status into MonitorRoom.
+	 * 
+	 * @return
+	 */
+	boolean register(IAlertBox alertbox) ;
+
+}
