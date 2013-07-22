e4c3f685944243fdb9de69d21dfc4b8a2c510d7b
rcsv <Awane.Tomohiro@me.com>
2013-07-22 15:41:03 +0900

    reconfigure tab and spaces.

--- src/org/rcsvp/factory/impl/Tolerance.java
+++ src/org/rcsvp/factory/impl/Tolerance.java
@@ -35,64 +35,69 @@ import org.rcsvp.factory.ITolerance.Type ;
  */
 public class Tolerance implements ITolerance {
 
-  // -----------------------------------------------------------------------
+	// -----------------------------------------------------------------------
 	// Private members
 	// -----------------------------------------------------------------------
-  
-  /**
-   * Tolerance Type
-   */
-  private Type type ;
-  
-  /**
-   * offset.
-   */
-  private double offset ;
-  
-  /**
-   * tolerance range.
-   */
-  private double tolerance ;
-  
-  // -----------------------------------------------------------------------
-  // CONSTRUCTOR
-  // -----------------------------------------------------------------------
-  
-  /**
-   * Construct with three arguments: ToleranceType, offset and range.
-   *
-   * @param type 
-   *    a Type of Tolerance category. Scantling, Geometric, etc.
-   * @param offset
-   *    a floating value of offet
-   * @param tolerance
-   *    a floating value of tolerance range
-   */
-  public Tolerance ( Type type, double offset, double tolerance ) {
-  
-    this.type = type ;
-    this.offset = offset ;
-    this.tolerance = tolerance ;
-    
-  }
-  
-  // -----------------------------------------------------------------------
-  // Override methods
-  // -----------------------------------------------------------------------
-  
-  @Override
-  public double getToleranceRange () {
-    return tolerance ;
-  }
-  
-  @Override
-  public double getOffset () {
-    return offset ;
-  }
-  
-  @Override
-  public Type getType () {
-    return type ;
-  }
+
+	/**
+	 * Tolerance Type
+	 */
+	private Type type ;
+
+	/**
+	 * offset.
+	 */
+	private double offset ;
+
+	/**
+	 * tolerance range.
+	 */
+	private double tolerance ;
+
+	// -----------------------------------------------------------------------
+	// CONSTRUCTOR
+	// -----------------------------------------------------------------------
+	
+	/**
+	 * Construct with three arguments: ToleranceType, offset and range.
+	 * 
+	 * @param type 
+	 * 		a Type of Tolerance category. Scantling, Geometric, etc.
+	 * @param offset
+	 * 		a floating value of offet
+	 * @param tolerance
+	 * 		a floating value of tolerance range
+	 */
+	 public Tolerance ( Type type, double offset, double tolerance ) {
+
+	 	this.type = type ;
+	 	this.offset = offset ;
+	 	this.tolerance = tolerance ;
+
+	 }
+
+	 // -----------------------------------------------------------------------
+	 // Override methods
+	 // -----------------------------------------------------------------------
+	 @Override
+	 public double getToleranceRange () {
+
+	 	return tolerance ;
+
+	 }
+
+	@Override
+	public double getOffset () {
+		
+		return offset ;
+
+	}
+
+	@Override
+	public Type getType () {
+
+		return type ;
+
+	}
   
 }
